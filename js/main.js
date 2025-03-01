3f30358f68d75c3ff528664c368f466b02bf6f05
AtharvaPC <baodhankaratharva@gmail.com>
2025-03-01 15:35:14 +0530

    Added navbar

--- js/main.js
+++ js/main.js
@@ -25,34 +25,60 @@ menuToggle.addEventListener("click", () => {
     menuToggle.classList.remove("closed");
     menuToggle.classList.add("opened");
     isAnimating = true;
+    
+    // Enhanced open animation
     gsap.to(menu, {
       clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
       ease: "hop",
       duration: 1.5,
       onStart: () => {
         menu.style.pointerEvents = "all";
-      },
+      }
+    });
+
+    // Animate menu items
+    gsap.from(".link, .social", {
+      y: 100,
+      opacity: 0,
+      stagger: 0.1,
+      duration: 1,
+      ease: "power4.out",
+      delay: 0.5,
       onComplete: () => {
         isAnimating = false;
-      },
+      }
     });
-  }
-  else {
+  } else {
     menuToggle.classList.remove("opened");
     menuToggle.classList.add("closed");
     isAnimating = true;
 
+    // Animate menu items out
+    gsap.to(".link, .social", {
+      y: -100,
+      opacity: 0,
+      stagger: 0.05,
+      duration: 0.5,
+      ease: "power2.in"
+    });
+
+    // Enhanced close animation
     gsap.to(menu, {
       clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
       ease: "hop",
       duration: 1.5,
+      delay: 0.3,
       onComplete: () => {
         menu.style.pointerEvents = "none";
         gsap.set(menu, {
-          clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+          clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)"
+        });
+        // Reset menu items position
+        gsap.set(".link, .social", {
+          clearProps: "all"
         });
         isAnimating = false;
-      },
+      }
     });
   }
 });
