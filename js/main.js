c1be9392cc64901c03ffc0ec6002b34c16dd6cdf
AtharvaPC <baodhankaratharva@gmail.com>
2025-03-01 16:33:12 +0530

    marquee

--- js/main.js
+++ js/main.js
@@ -82,3 +82,47 @@ menuToggle.addEventListener("click", () => {
     });
   }
 });
+
+
+
+
+// Marquee
+
+function marquee() {
+  let currentScroll = 0;
+  let isScrollingDown = true;
+  let arrows = document.querySelectorAll(".arrow");
+
+  let tween = gsap
+    .to(".marquee_part", {
+      xPercent: -100,
+      repeat: -1,
+      duration: 10,
+      ease: "linear",
+    })
+    .totalProgress(0.5);
+  gsap.set(".marquee_inner", { xPercent: -50 });
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > currentScroll) {
+      isScrollingDown = true;
+    } else {
+      isScrollingDown = false;
+    }
+    gsap.to(tween, {
+      timeScale: isScrollingDown ? 1 : -1,
+    });
+
+    arrows.forEach((arrow) => {
+      if (isScrollingDown) {
+        arrow.classList.remove("active");
+      } else {
+        arrow.classList.add("active");
+      }
+    });
+
+    currentScroll = window.pageYOffset;
+  });
+}
+
+marquee();
