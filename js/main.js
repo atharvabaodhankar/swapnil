c4057de56e8e5b36336baf97e9c1c6079ec856ba
AtharvaPC <baodhankaratharva@gmail.com>
2025-03-07 08:52:59 +0530

    added timeline init

--- js/main.js
+++ js/main.js
@@ -169,3 +169,18 @@ gsap.utils.toArray(".parallax-img-container").forEach(function(container) {
     ease: "none"
   });
 
+  let sections = gsap.utils.toArray(".timeline");
+
+  gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#timeline",
+      pin: true,
+      scrub: 1,
+      snap: 1 / (sections.length - 1),
+      end: () => "+=" + document.querySelector("#timeline").offsetWidth
+    }
+  });
+  
+  
